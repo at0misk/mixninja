57cac5108b31657da3b27e43dc1de6842f731723
Chris Lee <chris.leeau@me.com>
2016-05-24 13:24:09 -0700

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 
-  # devise_for :users, controllers: { sessions: 'users/sessions' }
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  # devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   get 'users/index'
 

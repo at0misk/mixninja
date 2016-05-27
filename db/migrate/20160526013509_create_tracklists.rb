2676538386604ebe16751fc6ec42bbf354b3299b
Kevin Taylor <ktp925@gmail.com>
2016-05-27 11:32:15 -0700

    Add files via upload

---
+++ db/migrate/20160526013509_create_tracklists.rb
@@ -0,0 +1,9 @@
+class CreateTracklists < ActiveRecord::Migration
+  def change
+    create_table :tracklists do |t|
+      t.integer :track_id
+      t.string :tracks
+      t.timestamps null: false
+    end
+  end
+end

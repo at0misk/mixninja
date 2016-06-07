9a04558433cb1305f96fd2af9a017b5d5ad66b0b
Kevin Taylor <kevintaylor@Kevins-MacBook-Pro.local>
2016-06-07 16:41:37 -0700

    Fixed turbolink bug

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :track
-  self.primary_keys = :user_id,:track_id
-  def track_by_most_likes
-  	Track.find_by_sql("SELECT tracks.title, COUNT(likes.id) AS num_of_likes FROM tracks JOIN likes ON likes.track_id = tracks.id GROUP BY tracks.id ORDER BY num_of_likes DESC")
-  end
 end
 
 
